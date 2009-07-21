3ee7560bef38a3c7ae5fd14d6155e75f645bf6a0
dalke <none@none>
2009-07-21 17:02:12 +0200

    removed some debugging code

--- test/test_demo_modules.py
+++ test/test_demo_modules.py
@@ -5,8 +5,6 @@ import urllib2
 
 def test_luckygoogle():
     url = server() + "akara.luckygoogle?q=google"
-    raise SystemExit(url)
-    
     f = urllib2.urlopen(url)
     s = f.read()
     assert s == "http://www.google.com/\n", repr(s)
