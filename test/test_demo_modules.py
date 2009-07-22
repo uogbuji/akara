6018bc398a470a339e30294d0702b29a5446c35d
dalke <none@none>
2009-07-23 01:42:38 +0200

    tests for markuptools

--- test/test_demo_modules.py
+++ test/test_demo_modules.py
@@ -55,7 +55,7 @@ def test_ical2json():
     url = server() + "ical.json"
 
     req = urllib2.Request(url)
-    req.add_header('Content-Type', 'test/calendar')
+    req.add_header('Content-Type', 'text/calendar')
 
     data = open(ical_filename).read()
 
@@ -66,6 +66,39 @@ def test_ical2json():
     assert len(items) == 2
     assert items[0]["summary"] == "Bastille Day Party"
     assert items[1]["summary"] == "Akara test"
+
+# markuptools.py
+
+def test_akara_twc():
+    url = server() + "akara.twc?max=5" # max 5 words in the result
+
+    req = urllib2.Request(url)
+    req.add_header('Content-Type', 'application/xml')
+
+    data = "<a>one two <b>three four </b><c>five <d>six seven</d> eight</c> nine</a>"
+    
+    result = urllib2.urlopen(req, data).read()
+    assert result == """\
+<?xml version="1.0" encoding="UTF-8"?>
+<html><head/><body><a>one two <b>three four </b><c>five </c></a></body></html>""", repr(result)
+
+# NOTE: the underlying trim code is NOT correct. This checks the invalid output
+# for consistency. See trac #11.
+def test_akara_twc_html():
+    url = server() + "akara.twc?html=yes" # max 500 words
+
+    req = urllib2.Request(url)
+    req.add_header('Content-Type', 'application/xml')
+
+    data = ("<html><head/><body>" + " ".join(map(str, range(510))) + "</body></html>")
+    
+    result = urllib2.urlopen(req, data).read()
+    assert result == """\
+<?xml version="1.0" encoding="UTF-8"?>
+<html><head/><body>0 1 2 3 4 5 6 7 8 9</body></html>""", repr(result)
+
+
+
 
 
 if __name__ == "__main__":
