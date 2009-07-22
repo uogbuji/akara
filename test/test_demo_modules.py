1321827a3ab52324ce0fde184252e2cdbc56232a
dalke <none@none>
2009-07-22 23:56:23 +0200

    Added ical2json tests. Liked "response" better than "f" as the result of urlopen

--- test/test_demo_modules.py
+++ test/test_demo_modules.py
@@ -1,5 +1,6 @@
 from server_support import server
 
+import urllib, urllib2
 from urllib2 import urlopen
 from amara import bindery
 from amara.tools import atomtools
@@ -7,32 +8,32 @@ from amara.tools import atomtools
 # luckygoogle.py
 def test_luckygoogle():
     url = server() + "akara.luckygoogle?q=google"
-    f = urlopen(url)
-    s = f.read()
+    response = urlopen(url)
+    s = response.read()
     assert s == "http://www.google.com/\n", repr(s)
 
 # atomtools.py
 def test_atom_json():
     import simplejson
     url = server() + "akara.atom.json?url=http://zepheira.com/feeds/news.atom"
-    f = urlopen(url)
-    results = simplejson.load(f)
+    response = urlopen(url)
+    results = simplejson.load(response)
     items = results["items"]
     for item in items:
         assert "title" in item
 
 def test_aggregate_atom():
     url = server() + "akara.aggregate.atom"
-    f = urlopen(url)
-    doc = bindery.parse(f, model=atomtools.FEED_MODEL)
+    response = urlopen(url)
+    doc = bindery.parse(response, model=atomtools.FEED_MODEL)
     assert str(doc.feed.title[0]) == "Feed me!", str(doc.feed.title[0])
     assert len(doc.feed.entry) == 3, len(doc.feed.entry)
 
 def test_webfeedjson():
     import simplejson
     url = server() + "akara.webfeed.json?url=http://feeds.delicious.com/v2/rss/recent%3Fmin=1%26count=15"
-    f = urlopen(url)
-    results = simplejson.load(f)
+    response = urlopen(url)
+    results = simplejson.load(response)
     print results
 
 # calweb.py
@@ -43,6 +44,20 @@ def test_calendar():
     s = urlopen(url).read()
     assert "bcCalendarToday" in s
 
+
+# icaltools.py
+def test_ical2json():
+    import simplejson
+    url = server() + "ical.json"
+    req = urllib2.Request(url)
+    req.add_header('Content-Type', 'test/calendar')
+    data = open("/Users/dalke/cvses/akara/test/resource/icalendar_test.ics").read()
+    response = urllib2.urlopen(req, data)
+    results = simplejson.load(response)
+    items = results["items"]
+    assert len(items) == 2
+    assert items[0]["summary"] == "Bastille Day Party"
+    assert items[1]["summary"] == "Akara test"
 
 
 if __name__ == "__main__":
