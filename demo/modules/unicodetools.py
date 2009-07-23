bda46d8a4f60bebd38af76aa0f13fcc057f4923c
dalke <none@none>
2009-07-23 22:50:42 +0200

    cleaned up code. Fixed bugs:  - tried to get the url from the tr instead of the tr.a  - didn't handle relative URLs; now make them absolute

--- demo/modules/unicodetools.py
+++ demo/modules/unicodetools.py
@@ -4,40 +4,42 @@
 
 from __future__ import with_statement
 
-import sys, time
-import urllib, urllib2
-#from cgi import parse_qs
-#from cStringIO import StringIO
+import sys
+import urllib, urllib2, urlparse
+from cStringIO import StringIO
 from gettext import gettext as _
-from itertools import *
-from functools import *
 from unicodedata import *
-from contextlib import closing
 
 from amara.bindery import html
-from amara.lib.util import *
+#from amara.lib.util import *
 
 from akara.services import simple_service, response
+from amara.writers.struct import *
 
 NAME_REQUIRED = _("The 'name' query parameter is mandatory.")
+
+
+def get_first_item(items, errmsg):
+    if not items:
+        raise AssertionError(errmsg)
+    return items[0]
 
 SERVICE_ID = 'http://purl.org/akara/services/builtin/unicode.charbyname'
 #@simple_service('get', SERVICE_ID, 'akara.rdfa.json', 'application/json')
 @simple_service('GET', SERVICE_ID, 'akara.unicode.charbyname', 'text/plain')
 def charbyname(name=None):
     '''
-    name - the character name to be looked up
+    name - the Unicode character name to look up
     
     Sample request:
     curl "http://localhost:8880/akara.unicode.charbyname?name=DOUBLE+DAGGER"
     '''
-    name = first_item(name, next=partial(assert_not_equal, None, msg=NAME_REQUIRED))
+    name = get_first_item(name, NAME_REQUIRED)
     return lookup(name).encode('utf-8')
 
 
 Q_REQUIRED = _("The 'q' query parameter is mandatory.")
-UINFO_SEARCH_PATTERN = u"http://www.fileformat.info/info/unicode/char/search.htm?%s"
-UINFO_CHAR_BASE = u"http://www.fileformat.info/info/unicode/char/"
+UINFO_SEARCH_URL = u"http://www.fileformat.info/info/unicode/char/search.htm?preview=entity&"
 
 SERVICE_ID = 'http://purl.org/akara/services/builtin/unicode.search'
 @simple_service('GET', SERVICE_ID, 'akara.unicode.search', 'application/xml')
@@ -48,15 +50,18 @@ def charsearch(q=None):
     Sample request:
     curl "http://localhost:8880/akara.unicode.search?q=dagger"
     '''
-    q = first_item(q, next=partial(assert_not_equal, None, msg=Q_REQUIRED))
-    query = urllib.urlencode({'q': q, 'preview': 'entity'})
-    doc = html.parse(UINFO_SEARCH_PATTERN%query)
+    q = get_first_item(q, Q_REQUIRED)
+    query = urllib.urlencode({"q": q})
+    search_url = UINFO_SEARCH_URL + query
+    doc = html.parse(search_url)
     buf = StringIO()
     structwriter(indent=u"yes", stream=buf).feed(
     ROOT(
         E((u'characters'),
-            (E(u'character', {u'see-also': row.td[0].href, u'name': unicode(row.td[2])}, (unicode(row.td[3])))
-            for row in doc.xml_select(u'//*[@class="list"]//*[starts-with(@class, "row")]'))
+            (E(u'character', {u'see-also': urlparse.urljoin(search_url, row.td[0].a.href),
+                              u'name': unicode(row.td[2]) },
+               unicode(row.td[3]))
+             for row in doc.xml_select(u'//*[@class="list"]//*[starts-with(@class, "row")]'))
         )
     ))
     return buf.getvalue()
