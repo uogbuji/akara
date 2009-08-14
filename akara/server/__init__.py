4905eecb1da4191088de59390e04c74599a76290
dalke <none@none>
2009-08-14 03:08:07 +0200

    Slightly more optimized

--- akara/server/__init__.py
+++ akara/server/__init__.py
@@ -372,10 +372,10 @@ class process(object):
             (kill, 0.5), (cont, 0.0)):
 
             servers = [ server for server in servers if server.active ]
-            if not servers:
-                break
             for server in servers:
                 action(server)
+            if not servers:
+                break
 
             _sleep(duration)
 
