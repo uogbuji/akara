649e078b84aa2a356ac29fab882f3da45f2971fd
dalke <none@none>
2009-10-16 20:04:52 +0200

    An example of how to dispatch based on different HTTP methods

---
+++ demo/modules/service_dispatcher.py
@@ -0,0 +1,21 @@
+from akara.services import service_dispatcher
+
+@service_dispatcher("http://purl.org/spam", "eggs")
+def vikings():
+    """Sing the Viking song
+
+    GET: [word=string] returns the song for that word
+    POST: returns some other text
+    """
+
+@vikings.simple_method("GET", "text/plain")
+def vikings_get(word):
+    word = word[0]
+    yield "%s, %s, %s, %s\n" % (word, word, word, word)
+    yield "%s, %s, %s, %s\n" % (word, word, word, word)
+    yield "%s-itty %s!\n" % (word, word)
+
+@vikings.method("POST")
+def vikings_post(environ, start_response):
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return "That was interesting.\n"
