70d6b0c32f90f1273609f0761b1b9b2d1a62ac83
dalke <none@none>
2009-10-18 02:48:09 +0200

    Changes to support storing the code under 'lib/' instead of 'akara'. Added 'akara' as a script to install.

--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python
 
 try:
+    # Installation goes better with setuptools
     from setuptools import setup, find_packages
+
     setuptools_extras = dict(
-        packages=find_packages(),
         zip_safe=False,
+        #packages = packages,
         #install_requires='Amara>=2.0a0',
         #tests_require='Paste>=1.0',
         )
 
 except ImportError:
+    # Workaround for standard distutils
+    import os
     from distutils.core import setup
+
     setuptools_extras = {}
+    def find_packages():
+        found = []
+        for root, dirs, files in os.walk("lib"):
+            if "__init__.py" in files:
+                found.append(root.replace("/", ".").replace("\\", "."))
+        found.sort()
+        return found
+
+# The module files are under "lib/". I want them under "akara/"
+def renamed_packages():
+    names = []
+    for package in find_packages():
+        if package.startswith("lib"):
+            names.append(package.replace("lib", "akara"))
+        else:
+            raise AssertionError("Unknown package %r" % (package,))
+    return names
+
+setuptools_extras["packages"] = renamed_packages()
 
 setup(name = "akara",
       version = "2.0a0",
@@ -19,5 +43,7 @@ setup(name = "akara",
       author='Uche Ogbuji and others',
       author_email='amara-dev@googlegroups.com',
       url='http://wiki.xml3k.org/Akara',
+      package_dir={'akara':'lib'},
+      scripts =['akara'],
       **setuptools_extras)
 
