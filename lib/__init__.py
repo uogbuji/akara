486e8f4aafbceb400244a420cd9f5f002235ac53
dalke <none@none>
2009-10-20 23:46:44 +0200

    added comments

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,16 @@
-# Akara
+"""akara - top-level module for an Akara installation
+
+Public submodules are:
+  registry - direct interface to the resource handler registry
+  services - decorators to simplify common ways of writing
+       and registring resource handlers
+
+     These modules contain data which change for each HTTP request.
+     They are used by some of the services decorators.
+  request - information about an incoming HTTP query
+  response - data for the outgoing HTTP response
+
+"""
 
 # Initializes logging and make the logger public
 from akara.logger_config import _logger as logger
