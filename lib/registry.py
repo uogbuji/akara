c5b878c3e6323d61519fb46b40b1feb12a5f7fa5
dalke <none@none>
2009-10-21 02:27:26 +0200

    Added comments and a bit of code cleanup

--- lib/registry.py
+++ lib/registry.py
@@ -1,4 +1,8 @@
-"Service registry"
+"""Service registry
+
+Add WSGI handlers to the Akara HTTP dispatch registry.
+
+"""
 
 import inspect
 
@@ -6,28 +10,39 @@ from amara import tree
 
 from akara import logger
 
+__all__ = ("register_service", "get_service")
 
 #### Simple registry of services
 
 
-# XXX call it 'path' or call it 'mount_point'?
+# We had some discussion about using the term 'path' or 'mount_point'?
 # Right now we don't really have '/' paths so there's no real difference
-# For more complex paths we might want to use Routes or similar system,
-# in which case we'll also have pattern matching on the path segments.
+# Therefore, you register mount_points, which must not have a "/" in them.
+# Incoming requests have a path, and the first segment (before the "/")
+# is used to find the mount point.
+
+# We're calling it a 'path' for future compatibility.  For more
+# complex paths we might want to use Routes or similar system, in
+# which case we'll also have pattern matching on the path segments.
+# You'll likely register a path pattern, still with the name 'path'.
 
 class Service(object):
+    "Internal class to store information about a given service resource"
     def __init__(self, handler, path, ident, doc):
         self.handler = handler # most important - the function to call
-        self.path = path  # location on the local service
+        self.path = path  # where to find the service
         self.ident = ident  # URN which identifies this uniquely
         self.doc = doc  # description to use when listing the service
 
 class Registry(object):
+    "Internal class to handle resource registration information"
     def __init__(self):
         self._registered_services = {}
         self.register_service(self.list_services,
                               "http://purl.org/xml3k/akara/services/builtin/registry", "")
     def register_service(self, handler, ident, path, doc=None):
+        if "/" in mount_point:
+            raise TypeError("Registered path may not contain a '/'")
         if doc is None:
             doc = inspect.getdoc(handler) or ""
         if ident in self._registered_services:
@@ -47,8 +62,6 @@ class Registry(object):
                 continue
             service_node = services.xml_append(tree.element(None, 'service'))
             service_node.xml_attributes['ident'] = service.ident
-            # XXX Is this 'path' or 'mount_point'?
-            # XXX if we move to Routes, how will we identify paths based on pattern?
             E = service_node.xml_append(tree.element(None, 'path'))
             E.xml_append(tree.text(path))
             E = service_node.xml_append(tree.element(None, 'description'))
@@ -66,9 +79,8 @@ _current_registry = Registry()
 def register_service(function, ident, path, doc=None):
     _current_registry.register_service(function, ident, path, doc)
 
-def get_service(path):
-    return _current_registry.get_service(path)
+def get_service(mount_point):
+    return _current_registry.get_service(mount_point)
 
 def list_services(ident=None):
     return _current_registry.list_services(ident)
-
