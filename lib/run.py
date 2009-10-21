3fa90c5eff8bf20e3fcf2ca42b8547600eed9451
dalke <none@none>
2009-10-21 04:59:59 +0200

    Support '-f' option to skip checking for an existing PID file. Added comments and documentation. Improved logging messages. Removed debugging code which no longer works.

--- lib/run.py
+++ lib/run.py
@@ -1,21 +1,22 @@
-"""Start up an Akara server on the command-line"""
-from __future__ import absolute_import
+"""Start up an Akara server on the command-line
 
+This is an internal module not for use by other libraries.
 
+"""
 import os
 import sys
-
 import socket
+import logging
+import signal
 
 from akara import read_config
-import logging
-
 from akara import logger, logger_config
 from akara.multiprocess_http import AkaraPreforkServer, load_modules
 
 
 def save_pid(pid_file):
-    # Newline terminated
+    "Save the current pid to the given PID filename"
+    # One line, newline terminated
     pid_s = str(os.getpid()) + "\n"
 
     try:
@@ -33,6 +34,7 @@ def save_pid(pid_file):
         f.close()
 
 def remove_pid(pid_file):
+    "Remove the given filename (which should be the PID file)"
     try:
         os.remove(pid_file)
     except Exception, error:
@@ -43,8 +45,22 @@ def remove_pid(pid_file):
         logger.info("Removed PID file %r", pid_file)
 
 
-# Used to coordinate between the parent and child processes.
-# The child needs to tell the parent if it could start up or not.
+# There are two ways to run the Akara server, either in debug mode
+# (running in the foreground, with the -X option) or in daemon mode
+# (running in the background) which is the default. The latter is
+# trickier to support.
+
+# In that case the command-line program spawns off a new process,
+# which is the master HTTP node ("the flup server"). It manages the
+# subprocesses which actually handle the HTTP requests. The flup
+# server starts up and either manages to set things up or fails
+# because of some problem. The command-line program needs to exit with
+# an error code if there was a problem, so there must be some sort of
+# communications between the two.
+
+# The solution is simple. Setup a pipe. The child sends either
+# "success\n" or "failure\n" as appropriate. The parent (which is the
+# command-line program) waits until it gets one of those messages.
 class NotifyParent(object):
     def __init__(self):
         self.r_pipe, self.w_pipe = os.pipe()
@@ -61,8 +77,8 @@ class NotifyParent(object):
         os.close(self.w_pipe)
         return status
 
-# Used in the "parent" when there is no child
-# and 
+# Life is much easier in debug mode. There's no need to communicate
+# anything to the non-existent parent.
 class NoParent(object):
     def failure(self):
         pass
@@ -74,7 +90,7 @@ def demonize():
     notify_parent = NotifyParent()
 
     if os.fork():
-        # In the parent. Wait for child status.
+        # In the command-line parent. Wait for child status.
         status = notify_parent.read_and_close()
 
         if status.startswith("success"):
@@ -82,6 +98,7 @@ def demonize():
         else:
             raise SystemExit(1)
 
+    # In the child, which is the flup server.
     try:
         # Create a new session with this process as the group leader
         try:
@@ -99,15 +116,23 @@ def demonize():
 def main(args):
     config_filename = args.config_filename
     debug = args.debug
+    skip_pid_check = args.skip_pid_check
 
     first_time = True
     old_server_address = None
     sock = None
     while 1:
-        # XXX comment here
+        # This is the main loop for the flup server.
+
+        # Why is it a loop? A SIGHUP sent to the server
+        # will shut down flup then reread the configuration
+        # file, reload the extension modules, and start
+        # the flup server again.
+
         settings, config = read_config.read_config(config_filename)
 
-        # For now, keep with the old Akara mechanism.
+        # In debug mode (-X), display all log messages.
+        # Otherwise, use the configuration level
         if debug:
             logger.setLevel(logging.DEBUG)
         else:
@@ -121,14 +146,13 @@ def main(args):
         modules = load_modules(settings["module_dir"],
                                settings["server_root"], config)
 
-        # Hopefully helpful check.
-        # XXX Should this die if it sees an existing pid?
+        # Don't start if the PID file already exists.
         pid_file = settings["pid_file"]
-        if first_time and os.path.exists(pid_file):
-            logger.warning(
-                "Existing PID file %r will be overwritten. (Is another instance running?)"
-                % (pid_file,))
-
+        if first_time and (not skip_pid_check) and os.path.exists(pid_file):
+            msg = ("Akara PID file %r already exists. Is another Akara instance running?\n"
+                   "If not, remove the file or use the '-f' option to skip this check")
+            logger.fatal(msg % (pid_file,))
+            raise SystemExit(1)
 
         if debug or not first_time:
             notify_parent = NoParent()
@@ -138,7 +162,6 @@ def main(args):
             try:
                 notify_parent = demonize()
             except Exception, err:
-                # XXX more comments
                 # This can come from the parent or the child.
                 logger.critical("Cannot spawn HTTP server", exc_info=True)
                 raise SystemExit("Exiting - check the log file for details")
@@ -152,7 +175,7 @@ def main(args):
                 if sock is not None:
                     sock.close()
                 sock = socket.socket()
-                # XXX SO_REUSEADDR should be a setting
+                # XXX Should SO_REUSEADDR be a configuration setting?
                 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 host, port = settings["server_address"]
                 if host:
@@ -172,6 +195,7 @@ def main(args):
             # Why? Because the old algorithm would add/cull the server count
             # within a few check intervals (each about 1 second), so it
             # didn't have much long-term effect.
+            logger.info("Akara server is running")
             server = AkaraPreforkServer(
                 minSpare = settings["min_spare_servers"],
                 maxSpare = settings["max_spare_servers"],
@@ -195,7 +219,7 @@ def main(args):
 
         # Fully demonize - no more logging to sys.std*
         # Close the standard file descriptors.
-        # XXX Reroute sys.std* to the log file?
+        # Redirect sys.std* to the log file
         if first_time and not debug:
             logger_config.remove_logging_to_stderr()
             logger_config.redirect_stdio()
@@ -207,45 +231,13 @@ def main(args):
             # this is also the root of the call tree for the child
             # which handles the request. The child exits at some point.
             raise
-        # XXX Check for other exceptions?
 
-        # XXX Strange. Why didn't flup disable this alarm?
-        import signal
+        # Strange. Why didn't flup disable this alarm?
         signal.alarm(0)
     
         if not hupReceived:
-            logger.info("Shutting down")
+            logger.info("Akara server shutting down.")
             break
-        logger.info("Restarting")
+        logger.info("Akara server is restarting.")
         first_time = False
     remove_pid(pid_file)
-
-# This is debugging code I used when with a bug during
-# demonization with redirecting stdout/stderr to the logger.
-# When enabled, wrap 'main' and dump any exceptions to
-# a log file. Defaults to the current terminal.
-# NOTE: in normal operation there will be MULTIPLE exits.
-#  - one for the command-line program, which forks to
-#  - the master process manager, which forks to
-#  - each spawned HTTP listener/worker process
-if 0:
-    def _get_exception_filename():
-        # return "/dev/ttyp2"  # Replace this if you want a particular file
-        return os.popen("tty").readline().rstrip()
-
-    # Get a useful filename
-    _exception_filename = _get_exception_filename()
-
-    # Use a new name for the existing 'main'
-    _main = main
-
-    def main(argv):
-        try:
-            _main(argv)
-        except:
-            import traceback
-            f = open(_exception_filename, "a")
-            f.write("Exception trace from %s\n" % (os.getpid(),))
-            traceback.print_exc(file=f)
-            f.close()
-            raise
