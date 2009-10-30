c98d157e3a45d7b266f7c55dad484672411da4d8
dalke <none@none>
2009-10-30 23:09:51 +0100

    Fixes for bug #37

--- lib/services.py
+++ lib/services.py
@@ -25,7 +25,7 @@ __all__ = ("service", "simple_service", "method_dispatcher")
 ERROR_DOCUMENT_TEMPLATE = """<?xml version="1.0" encoding="ISO-8859-1"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<hmtl xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
   <title>%(reason)s</title>
 </head>
